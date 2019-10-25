678552c80deb4a2c24ec02e77ecc39c32a596d33
Vladimir <vova@mytnik.net>
2019-10-25 14:20:47 +0300

    Updates

---
+++ precache-manifest.4c0c59581bc42dd441a5a8c3a4b92cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0009493102223c1637a904d3f8eff06",
+    "url": "/mrpuer/jm_timer/index.html"
+  },
+  {
+    "revision": "559bc96924ad23506466",
+    "url": "/mrpuer/jm_timer/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "a806d3eef635fc708244",
+    "url": "/mrpuer/jm_timer/static/css/main.172a3b93.chunk.css"
+  },
+  {
+    "revision": "559bc96924ad23506466",
+    "url": "/mrpuer/jm_timer/static/js/2.dc9f801f.chunk.js"
+  },
+  {
+    "revision": "a806d3eef635fc708244",
+    "url": "/mrpuer/jm_timer/static/js/main.d150b329.chunk.js"
+  },
+  {
+    "revision": "b2b402097a97ed940f7b",
+    "url": "/mrpuer/jm_timer/static/js/runtime-main.21f3e3b2.js"
+  }
+]);
